3f26fc8aed81cbe9d9ea4a773b135d54712c8eb4
Administrator <pookokok@hotmail.com>
2025-11-26 17:11:48 +0800

    加入swagger文档

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -2,33 +2,35 @@ package com.example.demo.controller;
 
 import generator.domain.new_table;
 import generator.service.new_tableService;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 @SpringBootApplication
 @RestController
-@ComponentScan(basePackages = {
-        "com.example.demo", // 主项目包（保留默认扫描）
-        "generator.service" // Service接口及实现类所在包（必须加！）
-})
-@MapperScan("generator.mapper")
+@Tag(name = "用户管理与商品查询接口", description = "包含用户CRUD、商品模糊查询等核心业务接口")
+@RequestMapping("/api")
 public class TestController {
     @Autowired
     private new_tableService aaa;
 
 
-    @ApiOperation("测试test1")
+    @Operation(summary = "测试test1", description = "查询数据并返回问候语")
     @GetMapping("/test")
     public String hello(@RequestParam(value = "name", defaultValue = "test") String name) {
-        long count= aaa.count();
+       long count= aaa.count();//aba
+
+
         List<new_table> list = aaa.list();
         String str= String.format("<UNK>%s<UNK>", list.get(0).getA1());
         return String.format(str+"Hello %s!", name);
