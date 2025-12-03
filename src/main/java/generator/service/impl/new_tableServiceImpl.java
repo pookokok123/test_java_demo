47539eac7aa41ae80ab286222190fb9f253dc43b
pookokok123 <pookokok@hotmail.com>
2025-12-03 09:59:07 +0800

    mybatisplus加分页

--- src/main/java/generator/service/impl/new_tableServiceImpl.java
+++ src/main/java/generator/service/impl/new_tableServiceImpl.java
@@ -13,25 +13,22 @@ import org.springframework.stereotype.Service;
 
 
 /**
-* @author Administrator
-* @description 针对表【new_table】的数据库操作Service实现
-* @createDate 2025-11-25 17:10:32
-*/
+ * @author Administrator
+ * @description 针对表【new_table】的数据库操作Service实现
+ * @createDate 2025-11-25 17:10:32
+ */
 @Service
-public class new_tableServiceImpl extends ServiceImpl<new_tableMapper, new_table>
-    implements new_tableService{
+public class new_tableServiceImpl extends ServiceImpl<new_tableMapper, new_table> implements new_tableService {
 
     @Autowired
     private new_tableMapper tableMapper;
 
-    public IPage<new_table> GetPage(int pageNo, int pageSize)
-    {
-//        IPage<new_table> page = tableMapper.selectPage(new Page<new_table>(1, 10), null);
+    public IPage<new_table> GetPage(int pageNo, int pageSize) {
         LambdaQueryWrapper<new_table> queryWrapper = new LambdaQueryWrapper<new_table>()
-                .gt(new_table::getId, 5); // 条件：年龄大于18
-        IPage<new_table> PageWithCondition = tableMapper.selectPage(new Page<new_table>(pageNo, pageSize), queryWrapper);
+                .gt(new_table::getId, 5);
+        IPage<new_table> pageWithCondition = tableMapper.selectPage(new Page<new_table>(pageNo, pageSize), queryWrapper);
 
-        return PageWithCondition;
+        return pageWithCondition;
     }
 
 }
